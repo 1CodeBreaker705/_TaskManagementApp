c3b5b26f5192b6ec6425e96afcea4de78997ea46
1CodeBreaker705 <ranjansingh0661@gmail.com>
2025-10-23 12:56:09 +0530

    Update TaskView.jsx

--- Frontend/src/components/TaskView.jsx
+++ Frontend/src/components/TaskView.jsx
@@ -39,9 +39,9 @@ const TaskView = ({data,close}) => {
   return (
     <>
       <h3 className=' text-sm lg:text-xl font-bold text-black mb-3 w-full break-words'>{data.title}</h3>
-      <p className=' text-xs lg:text-base font-semi-bold text-zinc-700 mb-3 w-full break-words'>{data.description}</p>
+      <p className=' text-xs lg:text-lg font-semi-bold text-zinc-700 mb-3 w-full break-words'>{data.description}</p>
       <p className='mb-5 text-sm'>Category:   <span className={`${categoryClass}`}>{data.category}</span></p>
-      <p className='mb-3 text-sm'>Status:  <span className='text-xs lg:sm px-2 lg:px-3 py-1.5 rounded-full font-medium bg-gray-100 text-gray-700 border border-gray-300 shadow-sm '>{data.status}</span></p>
+      <p className='mb-3 text-sm'>Status:  <span className='text-xs lg:sm px-2 py-1 lg:px-3 lg:py-1.5 rounded-full font-medium bg-gray-100 text-gray-700 border border-gray-300 shadow-sm '>{data.status}</span></p>
       <div className="flex items-center justify-center">
       <button onClick={deleteHandler} disabled={loading} className='text-sm lg:text-normal flex items-center justify-center gap-x-2 bg-red-600 disabled:bg-red-700 text-white px-2 py-1 rounded shadow cursor-pointer'>Delete{loading?<CgSpinner className='text-xs lg:text-xl animate-spin'/>:<FaTrash/>}</button>
       </div>

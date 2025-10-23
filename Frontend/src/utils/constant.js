7d4759ea2d020b49c8f31f1276d625ae3e2a94e1
1CodeBreaker705 <ranjansingh0661@gmail.com>
2025-10-23 12:59:11 +0530

    Update constant.js

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,10 +1,10 @@
 export const taskCategories = {
-  "Study": " px-3 py-1 bg-blue-100 text-blue-600 rounded-full border border-blue-600",
-  "Professional": "px-3 py-1 bg-green-100 text-green-600 rounded-full border border-green-600",
-  "Health": "px-3 py-1 bg-red-100 text-red-600 rounded-full border border-red-600",
-  "Personal": "px-3 py-1 bg-yellow-100 text-yellow-600 rounded-full border border-yellow-600",
-  "Social": "px-3 py-1 bg-pink-100 text-pink-600 rounded-full border border-pink-600",
-  "Others": "px-3 py-1 bg-gray-100 text-gray-600 rounded-full border border-gray-600"
+  "Study": " px-2 text-sm lg:text-base lg:px-3 lg:py-1 bg-blue-100 text-blue-600 rounded-full border border-blue-600",
+  "Professional":" px- 2 text-sm lg:text-base lg:px-3 lg:py-1 bg-green-100 text-green-600 rounded-full border border-green-600",
+  "Health": " px-2 text-sm lg:text-base lg:px-3 lg:py-1 bg-red-100 text-red-600 rounded-full border border-red-600",
+  "Personal": "px-2 text-sm lg:text-base lg:px-3 lg:py-1 bg-yellow-100 text-yellow-600 rounded-full border border-yellow-600",
+  "Social": "px-2 text-sm lg:text-base lg:px-3 lg:py-1 bg-pink-100 text-pink-600 rounded-full border border-pink-600",
+  "Others": "px-2 text-sm lg:text-base lg:px-3 lg:py-1 bg-gray-100 text-gray-600 rounded-full border border-gray-600"
 }
 
 export const statusList=['New','Pending','Active','Completed','Cancelled']

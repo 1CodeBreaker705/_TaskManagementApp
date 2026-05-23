bd7d1eb42e922d8b22e0ae3ec25f53811977afc6
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-23 20:24:36 +0530

    Update TaskView.jsx

--- Frontend/src/components/TaskView.jsx
+++ Frontend/src/components/TaskView.jsx
@@ -41,18 +41,27 @@ const TaskView = ({data,close}) => {
           <div className="pb-16">
             <h3 className=' text-base lg:text-xl font-bold text-black mb-3 w-full break-words'>{data.title}</h3>
             <div className="quill-content mb-7 w-full lg:p-1" dangerouslySetInnerHTML={{ __html: data.description }}></div>
-             <div className="mt-6 text-sm text-gray-600 space-y-2 md:absolute md:bottom-8 md:left-4 ">
-              <p><span className="font-medium">Category:</span> {data.category}</p>
-              <p><span className="font-medium">Status:</span> {data.status}</p>
-              <p><span className="font-medium">Priority:</span> {data.priority}</p>
-              <p><span className="font-medium">Created:</span> {new Date(data.createdAt).toLocaleDateString()}</p>
-                {data.dueDate && (
-                  <p className="text-indigo-500">
-                    <span className="font-medium">Due:</span>{" "}
-                    {new Date(data.dueDate).toLocaleDateString()}
-                  </p>
-                )}
-            </div>
+                  {/* Left details */}
+                  <div className="mt-6 md:absolute md:bottom-8 md:left-4 text-sm text-gray-600 space-y-2">
+                    <p><span className="font-medium">Category:</span> {data.category}</p>
+                    <p><span className="font-medium">Status:</span> {data.status}</p>
+                    <p><span className="font-medium">Priority:</span> {data.priority}</p>
+                  </div>
+                  
+                  {/* Right dates */}
+                  <div className="mt-6 md:absolute md:bottom-8 md:right-4 text-sm text-gray-600 space-y-2">
+                    <p>
+                      <span className="font-medium">Created:</span>{" "}
+                      {new Date(data.createdAt).toLocaleDateString()}
+                    </p>
+                  
+                    {data.dueDate && (
+                      <p className="text-indigo-500">
+                        <span className="font-medium">Due:</span>{" "}
+                        {new Date(data.dueDate).toLocaleDateString()}
+                      </p>
+                    )}
+                  </div>
           </div>
           <div className="absolute bottom-4 left-0 right-0 flex items-center justify-center">  
           <button onClick={deleteHandler} disabled={loading} className='text-xs lg:text-base flex items-center justify-center gap-x-1 lg:gap-x-2 bg-red-600 disabled:bg-red-700 text-white px-1 lg:px-2 py-1 rounded shadow cursor-pointer transform transition duration-300 hover:scale-105' >Delete{loading?<CgSpinner className='text-xs lg:text-xl animate-spin'/>:<FaTrash className='text-sm lg:text-base'/>}</button>

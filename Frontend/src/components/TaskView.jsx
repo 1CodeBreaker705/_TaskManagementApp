85d417446b5e1bfec75bb5c19a7b9d7fa4500af6
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-28 19:35:34 +0530

    Update TaskView.jsx

--- Frontend/src/components/TaskView.jsx
+++ Frontend/src/components/TaskView.jsx
@@ -64,7 +64,7 @@ const TaskView = ({data,close}) => {
                   </div>
           </div>
           <div className="absolute bottom-4 left-0 right-0 flex items-center justify-center">  
-          <button onClick={deleteHandler} disabled={loading} className='text-xs lg:text-base flex items-center justify-center gap-x-1 lg:gap-x-2  px-1 lg:px-2 py-1 bg-red-500 text-white border border-red-500 md:bg-red-50 md:text-red-500 md:border-red-200 md:hover:bg-red-500 md:hover:text-white md:hover:border-red-500 disabled:bg-red-300 disabled:border-red-300 disabled:text-white rounded-lg shadow cursor-pointer transition duration-300  hover:scale-[1.02]' >Delete{loading?<CgSpinner className='text-xs lg:text-xl animate-spin'/>:<FaTrash className='text-sm lg:text-base'/>}</button>
+          <button onClick={deleteHandler} disabled={loading} className='text-xs lg:text-base flex items-center justify-center gap-x-1 lg:gap-x-2  px-1 lg:px-2 py-1 bg-red-500 text-white border border-red-500 md:bg-red-100 md:text-red-500 md:border-red-200 md:hover:bg-red-500 md:hover:text-white md:hover:border-red-500 disabled:bg-red-300 disabled:border-red-300 disabled:text-white rounded-lg shadow cursor-pointer transition duration-300  hover:scale-[1.02]' >Delete{loading?<CgSpinner className='text-xs lg:text-xl animate-spin'/>:<FaTrash className='text-sm lg:text-base'/>}</button>
           </div>
         </div>
     </>

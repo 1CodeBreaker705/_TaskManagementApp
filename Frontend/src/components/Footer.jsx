da0c369b107aef948e49a367dcbbe9bce82e1d15
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-30 02:09:59 +0530

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
     <div className='mt-50 bg-gray-200 py-10 text-center'>
-      <p className="text-center font-medium">Copyright@<span className='font-bold'>{new Date().getFullYear()}</span></p>
+      <p className="text-center font-medium"><span className='font-bold'>©{new Date().getFullYear()}</span> Taskin. All rights reserved.</p>
     </div>
     </>
   )

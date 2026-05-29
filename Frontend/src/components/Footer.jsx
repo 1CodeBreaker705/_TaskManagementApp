027e6a9d57cbb28cbb5ba2b35477470d8eaa8c2b
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-30 02:22:38 +0530

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-    <div className='mt-50 bg-gray-200 py-10 text-center border-t border-gray-300'>
+    <div className='mt-50 bg-gray-200 py-10 text-center '>
       <p className="text-center font-medium"><span className='font-bold'>© {new Date().getFullYear()}</span> Taskin. All rights reserved.</p>
     </div>
     </>

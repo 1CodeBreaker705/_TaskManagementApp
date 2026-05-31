e19a0c268736d8f59d09adb8bf7ab4292c390f07
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-06-01 01:00:11 +0530

    Update constant.js

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -3,7 +3,7 @@ export const taskCategories = {
   "Professional":" px-2 text-sm lg:text-base lg:px-3 lg:py-1 bg-green-50 text-green-500 rounded-full border border-green-400",
   "Health": " px-2 text-sm lg:text-base lg:px-3 lg:py-1 bg-red-50 text-red-500 rounded-full border border-red-400",
   "Personal": "px-2 text-sm lg:text-base lg:px-3 lg:py-1 bg-yellow-50 text-yellow-500 rounded-full border border-yellow-400",
-  "Social": "px-2 text-sm lg:text-base lg:px-3 lg:py-1 bg-pink-50 text-pink-50 rounded-full border border-pink-400",
+  "Social": "px-2 text-sm lg:text-base lg:px-3 lg:py-1 bg-pink-50 text-pink-500 rounded-full border border-pink-400",
   "Others": "px-2 text-sm lg:text-base lg:px-3 lg:py-1 bg-gray-50 text-gray-500 rounded-full border border-gray-400"
 }
 
